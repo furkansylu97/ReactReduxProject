cf1de315025eb09a45fffc73fa1456e0d05605e2
Furkan Soylu <furkansylu97@gmail.com>
2025-06-14 13:50:45 +0300

    Css edits were performed and normalize.css packages were uploaded to project

--- ReduxProject/src/components/Navbar.jsx
+++ ReduxProject/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 import { BsFillBasketFill } from "react-icons/bs";
 
 function Navbar() {
   return (
     <nav>
-        <div>
-            <h3>Course App</h3>
-            <div>
-                <p>0</p>
-            </div>
-            <BsFillBasketFill />
+      <div className="Navbar">
+        <h3>Course App</h3>
+        <div className="NavbarIconWrapper">
+          <div className="Navbarquantity">
+            <p>0</p>
+          </div>
+          <BsFillBasketFill className="ItemsIcon" />
         </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- ReduxProject/src/main.jsx
+++ ReduxProject/src/main.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { store } from "./components/Store.jsx";
 import { Provider } from "react-redux";
+import 'normalize.css';
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
