79480db80a9a36c9c8e3f3dd22decdb3a8c8e15e
Furkan Soylu <furkansylu97@gmail.com>
2025-06-14 13:10:12 +0300

    createSlice method usage was performed

--- ReduxProject/src/components/Store.jsx
+++ ReduxProject/src/components/Store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from '../control/CartSlice' 
 
 export const store = configureStore({
     reducer:{
-        
+        cart: cartReducer,
     }
 })
